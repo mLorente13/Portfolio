9d7610fbb649f251edee307d5f51db4283af1a09
Marc Lorente <mlorente2005@gmail.com>
2025-03-11 19:19:57 +0100

    Improve responsiveness on navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import { Link, Outlet } from "react-router";
-import NavbarLink from "./NavbarLink";
 import { useState } from "react";
 import { useEffect } from "react";
 
 export default function Navbar() {
     let link = window.location.pathname.split("/")[1];
     if (link === "") {
-        link = "home";
+        link = "about";
     }
     const [activeLink, setActiveLink] = useState(link);
     const [activeLinkPosition, setActiveLinkPosition] = useState(0);
+    const [isMobile, setIsMobile] = useState(false);
 
     function calcActiveLinkPosition() {
         const currentLink = document.getElementById(activeLink);
@@ -18,64 +18,168 @@ export default function Navbar() {
     }
 
     useEffect(() => {
+        if (window.screen.width < 768) {
+            setIsMobile(true);
+        }
         calcActiveLinkPosition();
     }, [activeLink]);
 
-    window.addEventListener("resize", calcActiveLinkPosition);
+    window.addEventListener("resize", () => {
+        calcActiveLinkPosition();
+        if (window.screen.width < 768) {
+            setIsMobile(true);
+        } else {
+            setIsMobile(false);
+        }
+    });
 
     return (
-        <main>
+        <div className="bg-[#191c35] text-white">
             <header className="flex items-center justify-center">
-                <nav className="fixed z-10 top-5 flex items-center mx-auto text-center bg-gray-100/50 backdrop-blur-md rounded-xl w-3/4 lg:w-1/2">
+                <nav className="fixed md:h-10 z-10 bottom-10 w-[90%] md:top-5 flex items-center mx-auto text-center bg-gray-100/50 backdrop-blur-md rounded-xl md:w-3/4 lg:w-2/5">
                     <ul className="flex w-full p-0 m-0">
                         <Link
-                            id="home"
+                            id="about"
                             to="/"
-                            className="w-1/4 inline-block py-2 text-decoration-none rounded-xl font-semibold box-border text-black"
+                            className="z-10 flex items-center justify-center w-1/4 py-4 md:py-2 text-decoration-none rounded-xl font-semibold box-border text-black"
                             onClick={(e) => {
                                 setActiveLink(e.target.id);
                             }}
                         >
-                            Home
+                            {isMobile ? (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className=" pointer-events-none icon icon-tabler icons-tabler-outline icon-tabler-user"
+                                >
+                                    <path
+                                        stroke="none"
+                                        d="M0 0h24v24H0z"
+                                        fill="none"
+                                    />
+                                    <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                                    <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                                </svg>
+                            ) : (
+                                "About"
+                            )}
                         </Link>
                         <Link
                             id="projects"
                             to="/projects"
-                            className="inline-block w-1/4 py-2 text-decoration-none rounded-xl font-semibold box-border text-black"
+                            className="flex items-center justify-center w-1/4 py-4 md:py-2 text-decoration-none rounded-xl font-semibold box-border text-black"
                             onClick={(e) => {
                                 setActiveLink(e.target.id);
                             }}
                         >
-                            Projects
+                            {isMobile ? (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="pointer-events-none icon icon-tabler icons-tabler-outline icon-tabler-code"
+                                >
+                                    <path
+                                        stroke="none"
+                                        d="M0 0h24v24H0z"
+                                        fill="none"
+                                    />
+                                    <path d="M7 8l-4 4l4 4" />
+                                    <path d="M17 8l4 4l-4 4" />
+                                    <path d="M14 4l-4 16" />
+                                </svg>
+                            ) : (
+                                "Projects"
+                            )}
                         </Link>
                         <Link
                             id="education"
                             to="/education"
-                            className="inline-block w-1/4 py-2 text-decoration-none rounded-xl font-semibold box-border text-black"
+                            className="flex items-center justify-center w-1/4 py-4 md:py-2 text-decoration-none rounded-xl font-semibold box-border text-black"
                             onClick={(e) => {
                                 setActiveLink(e.target.id);
                             }}
                         >
-                            Education
+                            {isMobile ? (
+                                <svg
+                                    className="pointer-events-none nav-icon icon icon-tabler icons-tabler-outline icon-tabler-user"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <path
+                                        stroke="none"
+                                        d="M0 0h24v24H0z"
+                                        fill="none"
+                                    />
+                                    <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
+                                    <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
+                                </svg>
+                            ) : (
+                                "Education"
+                            )}
                         </Link>
                         <Link
                             id="contact"
                             to="/contact"
-                            className="inline-block w-1/4 py-2 text-decoration-none rounded-xl font-semibold box-border text-black"
+                            className="flex items-center justify-center w-1/4 py-4 md:py-2 text-decoration-none rounded-xl font-semibold box-border text-black"
                             onClick={(e) => {
                                 setActiveLink(e.target.id);
                             }}
                         >
-                            Contact
+                            {isMobile ? (
+                                <svg
+                                    className="pointer-events-none nav-icon icon icon-tabler icons-tabler-outline icon-tabler-user"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <path
+                                        stroke="none"
+                                        d="M0 0h24v24H0z"
+                                        fill="none"
+                                    />
+                                    <path d="M8 9h8" />
+                                    <path d="M8 13h6" />
+                                    <path d="M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z" />
+                                </svg>
+                            ) : (
+                                "Contact"
+                            )}
                         </Link>
                     </ul>
                     <div
-                        className="z-[-10] absolute bottom-0 w-1/4 h-full rounded-xl duration-300 bg-gray-300/70"
+                        className="z-[-10] absolute bottom-0 w-1/4 h-full rounded-xl duration-300 bg-[#f3f3f3b3]"
                         style={{ left: `${activeLinkPosition}px` }}
                     ></div>
                 </nav>
             </header>
             <Outlet />
-        </main>
+        </div>
     );
 }
