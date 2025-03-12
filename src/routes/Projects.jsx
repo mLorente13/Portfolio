3483f9efcd1ef1f1e0fb45f5d54e7f43963abbff
Marc Lorente <mlorente2005@gmail.com>
2025-03-12 12:01:34 +0100

    Improve responsiveness on small screens in projects page

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,7 +1,151 @@
+import ProjectCard from "../components/ProjectCard";
+import HTML from "../components/icons/HTML";
+import CSS from "../components/icons/CSS";
+import JavaScript from "../components/icons/JavaScript";
+import TailwindCSS from "../components/icons/TailwindCSS";
+import PWA from "../components/icons/PWA";
+import Spring from "../components/icons/Spring";
+
 export default function Projects() {
+    const tags = {
+        javascript: {
+            name: "JavaScript",
+            class: "bg-yellow-500",
+            icon: JavaScript,
+        },
+        html: {
+            name: "HTML",
+            class: "bg-orange-500",
+            icon: HTML,
+        },
+        css: {
+            name: "CSS",
+            class: "bg-[#264DE4]",
+            icon: CSS,
+        },
+        tailwindcss: {
+            name: "Tailwind CSS",
+            class: "bg-slate-900",
+            icon: TailwindCSS,
+        },
+        pwa: {
+            name: "PWA",
+            class: "bg-zinc-950",
+            icon: PWA,
+        },
+        ai: {
+            name: "AI",
+            class: "bg-[#FF4081]",
+            icon: "",
+        },
+        spring: {
+            name: "Spring Boot",
+            class: "bg-green-900",
+            icon: Spring,
+        },
+        rest: {
+            name: "REST API",
+            class: "bg-[#FF5733]",
+            icon: "",
+        },
+    };
+
+    const projects = [
+        {
+            title: "Calculator",
+            description:
+                "Simple calculator built with HTML, CSS, and JavaScript.",
+            id: "calculator",
+            links: [
+                "https://github.com/mLorente13/Calculator",
+                "https://calculator-mlt.netlify.app",
+            ],
+            image: "/calculator.webp",
+            tags: [tags.html, tags.tailwindcss, tags.javascript, tags.pwa],
+        },
+        {
+            title: "Gallery",
+            description:
+                "Gallery App built with HTML, CSS, and JavaScript using the unsplash API.",
+            id: "gallery",
+            links: [
+                "https://github.com/mLorente13/LlenguatgesMarques23/tree/main/Fetch",
+                "https://llenguatge-marques.netlify.app/fetch/",
+            ],
+            image: "/gallery.webp",
+            tags: [tags.html, tags.css, tags.javascript],
+        },
+        {
+            title: "Weather App",
+            description:
+                "Weather App built with HTML, CSS, and JavaScript using the open weather API.",
+            id: "weather",
+            links: [
+                "https://github.com/mLorente13/Weather-App",
+                "https://mlt-weather-app.netlify.app/",
+            ],
+            image: "/weather-app.webp",
+            tags: [tags.html, tags.css, tags.javascript],
+        },
+        {
+            title: "Memory Game",
+            description: "Memory game built with HTML, CSS and JavaScript.",
+            id: "memory",
+            links: [
+                "https://github.com/mLorente13/Practica_Memory_Game",
+                "https://mlt-memory-game.netlify.app/",
+            ],
+            image: "/memory-game.webp",
+            tags: [tags.html, tags.css, tags.javascript, tags.pwa],
+        },
+        {
+            title: "Restaurant Page",
+            description:
+                "Restaurant Page built with HTML, TailwindCSS, and JavaScript.",
+            id: "restaurant",
+            links: [
+                "https://github.com/mLorente13/mlt-web-restaurante",
+                "https://tajerdoro.netlify.app/",
+            ],
+            image: "/restaurant.webp",
+            tags: [tags.html, tags.tailwindcss, tags.javascript],
+        },
+        {
+            title: "Maze Game",
+            description:
+                "Maze game with both front-end and back-end built with HTML, CSS, JavaScript, and Spring Boot.",
+            id: "maze",
+            links: [
+                "https://github.com/mLorente13/DES-Practica_Maze",
+                "https://maze-game-d8e9.onrender.com",
+            ],
+            image: "/maze.webp",
+            tags: [tags.html, tags.css, tags.javascript, tags.spring],
+        },
+        {
+            title: "Forum App",
+            description:
+                "Forum app backend built with Sping Boot using a REST API.",
+            id: "forum",
+            links: [
+                "https://github.com/mLorente13/DES-practica-forum",
+                "https://mlt-forum-app.netlify.app/",
+            ],
+            image: "/forum-app.webp",
+            tags: [tags.spring],
+        },
+    ];
+
     return (
-        <main className="flex items-center justify-center h-screen">
-            <h1 className="text-6xl">Projects</h1>
+        <main className="w-11/12 lg:w-2/3 mx-auto flex flex-col flex-wrap pt-15 pb-30 md:py-22 min-h-screen">
+            <h1 className="text-5xl font-bold">
+                Projects<span className="text-orange-400">.</span>
+            </h1>
+            <section className="w-full flex flex-wrap gap-10 mt-8">
+                {projects.map((project, index) => (
+                    <ProjectCard key={index} {...project} />
+                ))}
+            </section>
         </main>
     );
 }
