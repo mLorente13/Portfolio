ca50f61b59926a0ff49a1bb5043d40adad9314f6
Marc Lorente <mlorente2005@gmail.com>
2025-06-28 22:14:38 +0200

    feat: add scroll driven animations on projects section

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,110 +1,121 @@
 import PropTypes from "prop-types";
+import { useRef, useEffect, useState } from "react";
 
 export default function ProjectCard({
-    title,
-    description,
-    links,
-    image,
-    tags,
+  title,
+  description,
+  links,
+  image,
+  tags,
 }) {
-    return (
-        <article className="flex flex-wrap">
-            <img
-                className="rounded-t-lg md:rounded-tr-none md:rounded-l-lg mx-auto w-full sm:w-1/2"
-                src={image}
-                alt={title}
-            />
-            <section className="rounded-b-lg md:rounded-bl-none md:rounded-r-lg w-full sm:w-1/2 box-border mx-auto flex justify-between flex-col gap-2 p-5 bg-gray-950/60">
-                <header className="flex items-center justify-end gap-4">
-                    {links.map((link, index) => (
-                        <a
-                            href={link}
-                            key={link}
-                            className="flex items-center gap-2 p-2 rounded-md border-2 text-blue-500 hover:bg-blue-500/10 hover:border-blue-500/50 duration-200"
-                        >
-                            {index === 0 ? (
-                                <>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="24"
-                                        height="24"
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeWidth="2"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        className="w-6 h-6 icon icon-tabler icons-tabler-outline icon-tabler-brand-github"
-                                    >
-                                        <path
-                                            stroke="none"
-                                            d="M0 0h24v24H0z"
-                                            fill="none"
-                                        />
-                                        <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
-                                    </svg>
-                                    <span>Code</span>
-                                </>
-                            ) : (
-                                <>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="24"
-                                        height="24"
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeWidth="2"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        className="h-6 w-6 icon icon-tabler icons-tabler-outline icon-tabler-link"
-                                    >
-                                        <path
-                                            stroke="none"
-                                            d="M0 0h24v24H0z"
-                                            fill="none"
-                                        />
-                                        <path d="M9 15l6 -6" />
-                                        <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" />
-                                        <path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" />
-                                    </svg>
-                                    <span>Preview</span>
-                                </>
-                            )}
-                        </a>
-                    ))}
-                </header>
-                <section className="flex flex-wrap gap-2">
-                    <h2 className="w-full text-2xl font-bold">{title}</h2>
-                    <p className="w-full">{description}</p>
-                    {tags.map((tag) => (
-                        <span
-                            className={
-                                `flex items-center px-3 py-0.5 gap-2 rounded-2xl ` +
-                                tag.class
-                            }
-                            key={tag.name}
-                        >
-                            <tag.icon className="h-4.5 w-4.5" />
-                            {tag.name}
-                        </span>
-                    ))}
-                </section>
-            </section>
-        </article>
+  const [isVisible, setIsVisible] = useState(false);
+  const cardRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver(
+      ([entry]) => setIsVisible(entry.isIntersecting),
+      { threshold: 0.2 }
     );
+    if (cardRef.current) observer.observe(cardRef.current);
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <article
+      ref={cardRef}
+      className={`flex flex-wrap transition-all ease-out duration-700  ${
+        isVisible
+          ? "opacity-100 translate-x-0 translate-y-0"
+          : "opacity-0 translate-y-8"
+      }`}
+    >
+      <img
+        className="rounded-t-lg md:rounded-tr-none md:rounded-l-lg mx-auto w-full sm:w-1/2"
+        src={image}
+        alt={title}
+      />
+      <section className="rounded-b-lg md:rounded-bl-none md:rounded-r-lg w-full sm:w-1/2 box-border mx-auto flex justify-between flex-col gap-2 p-5 bg-gray-950/60">
+        <header className="flex items-center justify-end gap-4">
+          {links.map((link, index) => (
+            <a
+              href={link}
+              key={link}
+              className="flex items-center gap-2 p-2 rounded-md border-2 text-blue-500 hover:bg-blue-500/10 hover:border-blue-500/50 duration-200"
+            >
+              {index === 0 ? (
+                <>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="w-6 h-6 icon icon-tabler icons-tabler-outline icon-tabler-brand-github"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+                  </svg>
+                  <span>Code</span>
+                </>
+              ) : (
+                <>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="h-6 w-6 icon icon-tabler icons-tabler-outline icon-tabler-link"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M9 15l6 -6" />
+                    <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" />
+                    <path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" />
+                  </svg>
+                  <span>Preview</span>
+                </>
+              )}
+            </a>
+          ))}
+        </header>
+        <section className="flex flex-wrap gap-2">
+          <h2 className="w-full text-2xl font-bold">{title}</h2>
+          <p className="w-full">{description}</p>
+          {tags.map((tag) => (
+            <span
+              className={
+                `flex items-center px-3 py-0.5 gap-2 rounded-2xl ` + tag.class
+              }
+              key={tag.name}
+            >
+              <tag.icon className="h-4.5 w-4.5" />
+              {tag.name}
+            </span>
+          ))}
+        </section>
+      </section>
+    </article>
+  );
 }
 
 ProjectCard.propTypes = {
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    links: PropTypes.arrayOf(PropTypes.string).isRequired,
-    image: PropTypes.string.isRequired,
-    tags: PropTypes.arrayOf(
-        PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            icon: PropTypes.elementType.isRequired,
-            class: PropTypes.string.isRequired,
-        })
-    ).isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  links: PropTypes.arrayOf(PropTypes.string).isRequired,
+  image: PropTypes.string.isRequired,
+  tags: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      icon: PropTypes.elementType.isRequired,
+      class: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -8,159 +8,157 @@ import Spring from "../components/icons/Spring";
 import ReactIcon from "../components/icons/React";
 
 export default function Projects() {
-    const tags = {
-        javascript: {
-            name: "JavaScript",
-            class: "bg-yellow-500/50",
-            icon: JavaScript,
-        },
-        html: {
-            name: "HTML",
-            class: "bg-orange-500/50",
-            icon: HTML,
-        },
-        css: {
-            name: "CSS",
-            class: "bg-blue-500/50",
-            icon: CSS,
-        },
-        tailwindcss: {
-            name: "Tailwind CSS",
-            class: "bg-[#06B6D4]/50",
-            icon: TailwindCSS,
-        },
-        pwa: {
-            name: "PWA",
-            class: "bg-zinc-950",
-            icon: PWA,
-        },
-        ai: {
-            name: "AI",
-            class: "bg-[#FF4081]",
-            icon: "",
-        },
-        spring: {
-            name: "Spring Boot",
-            class: "bg-green-900",
-            icon: Spring,
-        },
-        rest: {
-            name: "REST API",
-            class: "bg-[#FF5733]",
-            icon: "",
-        },
-        react: {
-            name: "React",
-            class: "bg-[#53C1DE]/20",
-            icon: ReactIcon,
-        },
-    };
+  const tags = {
+    javascript: {
+      name: "JavaScript",
+      class: "bg-yellow-500/50",
+      icon: JavaScript,
+    },
+    html: {
+      name: "HTML",
+      class: "bg-orange-500/50",
+      icon: HTML,
+    },
+    css: {
+      name: "CSS",
+      class: "bg-blue-500/50",
+      icon: CSS,
+    },
+    tailwindcss: {
+      name: "Tailwind CSS",
+      class: "bg-[#06B6D4]/50",
+      icon: TailwindCSS,
+    },
+    pwa: {
+      name: "PWA",
+      class: "bg-zinc-950",
+      icon: PWA,
+    },
+    ai: {
+      name: "AI",
+      class: "bg-[#FF4081]",
+      icon: "",
+    },
+    spring: {
+      name: "Spring Boot",
+      class: "bg-green-900",
+      icon: Spring,
+    },
+    rest: {
+      name: "REST API",
+      class: "bg-[#FF5733]",
+      icon: "",
+    },
+    react: {
+      name: "React",
+      class: "bg-[#53C1DE]/20",
+      icon: ReactIcon,
+    },
+  };
 
-    const projects = [
-        {
-            title: "Speedtrack",
-            description:
-                "Speedtrack is a web application for booking karting sessions and keep track of your performance.",
-            id: "speedtrack",
-            links: [null, "https://speed-track.es/"],
-            image: "/speedtrack.webp",
-            tags: [tags.react, tags.tailwindcss, tags.spring],
-        },
-        {
-            title: "Calculator",
-            description:
-                "Simple calculator built with HTML, CSS, and JavaScript.",
-            id: "calculator",
-            links: [
-                "https://github.com/mLorente13/Calculator",
-                "https://calculator-mlt.netlify.app",
-            ],
-            image: "/calculator.webp",
-            tags: [tags.html, tags.tailwindcss, tags.javascript, tags.pwa],
-        },
-        {
-            title: "Gallery",
-            description:
-                "Gallery App built with HTML, CSS, and JavaScript using the unsplash API.",
-            id: "gallery",
-            links: [
-                "https://github.com/mLorente13/LlenguatgesMarques23/tree/main/Fetch",
-                "https://llenguatge-marques.netlify.app/fetch/",
-            ],
-            image: "/gallery.webp",
-            tags: [tags.html, tags.css, tags.javascript],
-        },
-        {
-            title: "Weather App",
-            description:
-                "Weather App built with HTML, CSS, and JavaScript using the open weather API.",
-            id: "weather",
-            links: [
-                "https://github.com/mLorente13/Weather-App",
-                "https://mlt-weather-app.netlify.app/",
-            ],
-            image: "/weather-app.webp",
-            tags: [tags.html, tags.css, tags.javascript],
-        },
-        {
-            title: "Memory Game",
-            description: "Memory game built with HTML, CSS and JavaScript.",
-            id: "memory",
-            links: [
-                "https://github.com/mLorente13/Practica_Memory_Game",
-                "https://mlt-memory-game.netlify.app/",
-            ],
-            image: "/memory-game.webp",
-            tags: [tags.html, tags.css, tags.javascript, tags.pwa],
-        },
-        {
-            title: "Restaurant Page",
-            description:
-                "Restaurant Page built with HTML, TailwindCSS, and JavaScript.",
-            id: "restaurant",
-            links: [
-                "https://github.com/mLorente13/mlt-web-restaurante",
-                "https://tajerdoro.netlify.app/",
-            ],
-            image: "/restaurant.webp",
-            tags: [tags.html, tags.tailwindcss, tags.javascript],
-        },
-        {
-            title: "Maze Game",
-            description:
-                "Maze game with both front-end and back-end built with HTML, CSS, JavaScript, and Spring Boot.",
-            id: "maze",
-            links: [
-                "https://github.com/mLorente13/DES-Practica_Maze",
-                "https://maze-game-d8e9.onrender.com",
-            ],
-            image: "/maze.webp",
-            tags: [tags.html, tags.css, tags.javascript, tags.spring],
-        },
-        {
-            title: "Forum App",
-            description:
-                "Forum app backend built with Sping Boot using a REST API.",
-            id: "forum",
-            links: [
-                "https://github.com/mLorente13/DES-practica-forum",
-                "https://mlt-forum-app.netlify.app/",
-            ],
-            image: "/forum-app.webp",
-            tags: [tags.spring],
-        },
-    ];
+  const projects = [
+    {
+      title: "Speedtrack",
+      description:
+        "Speedtrack is a web application for booking karting sessions and keep track of your performance.",
+      id: "speedtrack",
+      links: [null, "https://speed-track.es/"],
+      image: "/speedtrack.webp",
+      tags: [tags.react, tags.tailwindcss, tags.spring],
+    },
+    {
+      title: "Calculator",
+      description: "Simple calculator built with HTML, CSS, and JavaScript.",
+      id: "calculator",
+      links: [
+        "https://github.com/mLorente13/Calculator",
+        "https://calculator-mlt.netlify.app",
+      ],
+      image: "/calculator.webp",
+      tags: [tags.html, tags.tailwindcss, tags.javascript, tags.pwa],
+    },
+    {
+      title: "Gallery",
+      description:
+        "Gallery App built with HTML, CSS, and JavaScript using the unsplash API.",
+      id: "gallery",
+      links: [
+        "https://github.com/mLorente13/LlenguatgesMarques23/tree/main/Fetch",
+        "https://llenguatge-marques.netlify.app/fetch/",
+      ],
+      image: "/gallery.webp",
+      tags: [tags.html, tags.css, tags.javascript],
+    },
+    {
+      title: "Weather App",
+      description:
+        "Weather App built with HTML, CSS, and JavaScript using the open weather API.",
+      id: "weather",
+      links: [
+        "https://github.com/mLorente13/Weather-App",
+        "https://mlt-weather-app.netlify.app/",
+      ],
+      image: "/weather-app.webp",
+      tags: [tags.html, tags.css, tags.javascript],
+    },
+    {
+      title: "Memory Game",
+      description: "Memory game built with HTML, CSS and JavaScript.",
+      id: "memory",
+      links: [
+        "https://github.com/mLorente13/Practica_Memory_Game",
+        "https://mlt-memory-game.netlify.app/",
+      ],
+      image: "/memory-game.webp",
+      tags: [tags.html, tags.css, tags.javascript, tags.pwa],
+    },
+    {
+      title: "Restaurant Page",
+      description:
+        "Restaurant Page built with HTML, TailwindCSS, and JavaScript.",
+      id: "restaurant",
+      links: [
+        "https://github.com/mLorente13/mlt-web-restaurante",
+        "https://tajerdoro.netlify.app/",
+      ],
+      image: "/restaurant.webp",
+      tags: [tags.html, tags.tailwindcss, tags.javascript],
+    },
+    {
+      title: "Maze Game",
+      description:
+        "Maze game with both front-end and back-end built with HTML, CSS, JavaScript, and Spring Boot.",
+      id: "maze",
+      links: [
+        "https://github.com/mLorente13/DES-Practica_Maze",
+        "https://maze-game-d8e9.onrender.com",
+      ],
+      image: "/maze.webp",
+      tags: [tags.html, tags.css, tags.javascript, tags.spring],
+    },
+    {
+      title: "Forum App",
+      description: "Forum app backend built with Sping Boot using a REST API.",
+      id: "forum",
+      links: [
+        "https://github.com/mLorente13/DES-practica-forum",
+        "https://mlt-forum-app.netlify.app/",
+      ],
+      image: "/forum-app.webp",
+      tags: [tags.spring],
+    },
+  ];
 
-    return (
-        <main className=" w-11/12 lg:w-2/3 2xl:w-1/2 mx-auto flex flex-col flex-wrap pt-15 pb-30 md:py-22 min-h-screen">
-            <h1 className="text-5xl font-bold">
-                Projects<span className="text-orange-400">.</span>
-            </h1>
-            <section className="w-full flex flex-wrap gap-10 mt-8">
-                {projects.map((project, index) => (
-                    <ProjectCard key={index} {...project} />
-                ))}
-            </section>
-        </main>
-    );
+  return (
+    <main className=" w-11/12 lg:w-2/3 2xl:w-1/2 mx-auto flex flex-col flex-wrap pt-15 pb-30 md:py-22 min-h-screen">
+      <h1 className="text-5xl font-bold">
+        Projects<span className="text-orange-400">.</span>
+      </h1>
+      <section className="w-full flex flex-wrap gap-10 mt-8">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} {...project} delay={index * 100} />
+        ))}
+      </section>
+    </main>
+  );
 }
